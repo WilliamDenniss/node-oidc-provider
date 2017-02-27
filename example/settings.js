c7ae89a313310771d65195ce871b67ce038a23ec
William Denniss <wdenniss@google.com>
2017-02-26 16:13:20 -0800

    Updated custom URI scheme tests. – Uses a single slash (there's no URI authority). – No host, so 'localhost' isn't needed.
    Enabled native app support by default in the example.

--- example/settings.js
+++ example/settings.js
@@ -35,6 +35,7 @@ module.exports.config = {
     revocation: true,
     sessionManagement: true,
     backchannelLogout: true,
+    oauthNativeApps: true,
   },
   subjectTypes: ['public', 'pairwise'],
   pairwiseSalt: 'da1c442b365b563dfc121f285a11eedee5bbff7110d55c88',
